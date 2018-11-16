ec2b35bed227222331367c1a36db9c9583562228
Aakash Goel <aakash.goel@nagarro.com>
2018-11-16 11:08:00 +0530

    minor fix

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -5,7 +5,7 @@
             <book-item v-for="book in books":book="book" :key="book.title"></book-item>
         </ul>
         <hr>
-        <book-form @addBook="appendBook"</book-form>
+        <book-form @addBook="appendBook"></book-form>
     </div>  
 </template>
 
