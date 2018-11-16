c8296f469fbfd5170773e364ddb7b77f722fd8f8
Aakash Goel <aakash.goel@nagarro.com>
2018-11-16 10:22:55 +0530

    Created BookItem child component

---
+++ src/components/BookItem.vue
@@ -0,0 +1,20 @@
+<template>
+    <li v-for="book in books">{{book.title}} :  {{book.author}}</li>
+</template>
+
+<script>
+export default {
+  name: "BookItem",
+  props: ["book"],
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style>
+li {
+  display: block;
+  margin: 0 10px;
+}
+</style>
